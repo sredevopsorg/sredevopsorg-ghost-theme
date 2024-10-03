0616169cea13ccd5a24f84dca7bb09df7d31742c
Nicolás Georger <info@sredevops.org>
2024-10-03 20:36:47 -0300

    prerelease

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,18 @@ document
     var menu = document.querySelector(".origin-top-right");
     menu.classList.toggle("hidden");
   });
+function openModal() {
+  document.getElementById("searchModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("searchModal").style.display = "none";
+}
+
+// Close the modal when the user clicks anywhere outside of it
+window.onclick = function (event) {
+  const modal = document.getElementById("searchModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
