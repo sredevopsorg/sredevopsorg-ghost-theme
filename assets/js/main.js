c4b14f20cc3724c865b93d681cfeb491d7597847
Nicolás Georger <info@sredevops.org>
2026-01-07 22:48:30 -0300

    feat: Add Spanish language support with custom templates and components
    - Implemented custom post template for Spanish content in .
    - Created default Spanish template in .
    - Updated home page to use Spanish posts list in .
    - Added author box component for Spanish in .
    - Introduced post card component for Spanish in .
    - Created posts list component for Spanish in .
    - Added tag page template for Spanish in .
    - Updated social share component to remove unnecessary text in .
    - Incremented version number in  to 1.4.2.
    - Commented out language detection script in .

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,21 +44,21 @@ window.onclick = function (event) {
 /* Set the language attribute on the HTML element based on the subpath of the URL
 This script will set the language attribute on the HTML element based on the subpath of the URL
 */
-document.addEventListener('DOMContentLoaded', function () {
-    var path = window.location.pathname.split('/')[1]; // Get the first subpath
-    var lang = 'en'; // Default language
-    // Map subpaths to locales
-    var localeMap = {
-        'es': 'es',
-        'br': 'pt',
-        'en': 'en',
-        // Add more mappings as needed
-    };
-    if (localeMap.hasOwnProperty(path)) {
-        lang = localeMap[path];
-    }
-    document.querySelector('html').setAttribute('lang', lang);
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     var path = window.location.pathname.split('/')[1]; // Get the first subpath
+//     var lang = 'en'; // Default language
+//     // Map subpaths to locales
+//     var localeMap = {
+//         'es': 'es',
+//         'br': 'pt',
+//         'en': 'en',
+//         // Add more mappings as needed
+//     };
+//     if (localeMap.hasOwnProperty(path)) {
+//         lang = localeMap[path];
+//     }
+//     document.querySelector('html').setAttribute('lang', lang);
+// });
 
 /* Responsive HTML table */
 (function () {
